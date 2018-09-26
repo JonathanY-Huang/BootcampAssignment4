f04a449c2d53453f1ac065d5b292145bd1bf061c
Jonathan Huang <32203695+JonathanY-Huang@users.noreply.github.com>
2018-09-25 23:14:26 -0400

    Uno mas

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://assignment4321.herokuapp.com/api/listings');
+      return $http.get('http://assignment4321.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('https://assignment4321.herokuapp.com/api/listings', listing);
+	  return $http.post('http://assignment4321.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-	return $http.delete('https://assignment4321.herokuapp.com/api/listings', id);
+	return $http.delete('http://assignment4321.herokuapp.com/api/listings', id);
     }
   };
 
