7d8623e2bae3245d837c283dea57bfa4d73bf4e8
Jonathan Huang <32203695+JonathanY-Huang@users.noreply.github.com>
2018-09-25 22:57:21 -0400

    fixed bug

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://assignment4321.herokuapp.com');
+      return $http.get('https://assignment4321.herokuapp.com/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('https://assignment4321.herokuapp.com', listing);
+	  return $http.post('https://assignment4321.herokuapp.com/listings', listing);
     }, 
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-	return $http.delete('https://assignment4321.herokuapp.com', id);
+	return $http.delete('https://assignment4321.herokuapp.com/listings', id);
     }
   };
 
