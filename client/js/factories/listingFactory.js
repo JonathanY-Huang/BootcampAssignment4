d1a0d635aac247785a4f199a6ff1b7a6308dbeb7
Jonathan Huang <32203695+JonathanY-Huang@users.noreply.github.com>
2018-09-25 23:11:50 -0400

    One more time for the people in the back

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://assignment4321.herokuapp.com/listings');
+      return $http.get('https://assignment4321.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('https://assignment4321.herokuapp.com/listings', listing);
+	  return $http.post('https://assignment4321.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-	return $http.delete('https://assignment4321.herokuapp.com/listings', id);
+	return $http.delete('https://assignment4321.herokuapp.com/api/listings', id);
     }
   };
 
